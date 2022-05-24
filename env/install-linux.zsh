1d7df5fc65af87bc4b8a7818790d44f2c7be7436
SlashNephy <slashnephy@hatena.ne.jp>
2022-05-24 18:29:35 +0900

    check distribution
    Signed-off-by: SlashNephy <slashnephy@hatena.ne.jp>

--- env/install-linux.zsh
+++ env/install-linux.zsh
@@ -17,3 +17,11 @@ printf "Install .nanorc? [y/N]: "
 if read -r -q; then
   echo; ln -sf ./linux/.nanorc ~/.nanorc
 fi
+
+# check distribution
+DISTRIBUTION=$( (lsb_release -ds || cat /etc/*release || uname -om) 2>/dev/null | head -n1)
+case "$DISTRIBUTION" in
+  "*Arch Linux*") ./install-macos.zsh ;;
+  *)              echo "Unsupported DISTRIBUTION: $DISTRIBUTION"
+                  exit 1 ;;
+esac
