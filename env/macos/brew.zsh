e121aaff9ddfcfd3aae0c5aa2528078432fdfb59
SlashNephy <slashnephy@hatena.ne.jp>
2022-05-27 01:11:10 +0900

    stubby
    Signed-off-by: SlashNephy <slashnephy@hatena.ne.jp>

--- env/macos/brew.zsh
+++ env/macos/brew.zsh
@@ -1,2 +1,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 sudo softwareupdate --install-rosetta
+
+brew bundle --file=~/.github/env/macos/homebrew/Brewfile
