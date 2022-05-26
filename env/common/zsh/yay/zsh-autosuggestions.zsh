8285a08a8009e87fe46c9b91c6e7bfd30857a1b9
SlashNephy <slashnephy@hatena.ne.jp>
2022-05-27 00:03:20 +0900

    yay
    Signed-off-by: SlashNephy <slashnephy@hatena.ne.jp>

---
+++ env/common/zsh/yay/zsh-autosuggestions.zsh
@@ -0,0 +1,2 @@
+# yay -S zsh-autosuggestions
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
--- env/install-arch.zsh
+++ env/install-arch.zsh
@@ -1,2 +1,7 @@
 #!/usr/bin/env zsh
 set -eu
+
+printf "Install yay? [y/N]: "
+if read -r -q; then
+  echo; zsh ./linux/yay.zsh
+fi
