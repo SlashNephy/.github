ab36c8de182cf47508bf66380b7480c63efb98f8
SlashNephy <slashnephy@hatena.ne.jp>
2022-05-26 06:00:05 +0000

    fix: use ~

--- env/install-linux.zsh
+++ env/install-linux.zsh
@@ -1,8 +1,6 @@
 #!/usr/bin/env zsh
 set -eu
 
-cd "$(dirname "$0")"
-
 printf "Install .ssh/config? [y/N]: "
 if read -r -q; then
   echo; ln -sf ~/.github/env/linux/.ssh/config ~/.ssh/config
@@ -21,8 +19,8 @@ fi
 # check distribution
 DISTRIBUTION=$( (lsb_release -ds || cat /etc/*release || uname -om) 2>/dev/null | head -n1)
 case "$DISTRIBUTION" in
-  "*Arch Linux*") ./install-arch.zsh ;;
-  "Ubuntu*")      ./install-ubuntu.zsh ;;
-  *)              echo "Unsupported DISTRIBUTION: $DISTRIBUTION"
-                  exit 1 ;;
+  "*Arch Linux*") ~/.github/env/install-arch.zsh ;;
+  "Ubuntu*")       ~/.github/env/install-ubuntu.zsh ;;
+  *)                    echo "Unsupported DISTRIBUTION: $DISTRIBUTION"
+                        exit 1 ;;
 esac
