a921c57d26f537e4af1481d5f04e8d30146c5b8c
SlashNephy <slashnephy@hatena.ne.jp>
2022-05-28 08:29:00 +0000

    add plugins

--- env/ubuntu/zsh/zplug-plugins.zsh
+++ env/ubuntu/zsh/zplug-plugins.zsh
@@ -3,6 +3,9 @@ zplug "zsh-users/zsh-history-substring-search"
 zplug "chrissicool/zsh-256color"
 zplug "mrowa44/emojify", as:command
 
+zplug "zsh-users/zsh-autosuggestions"
+zplug "zdharma-continuum/fast-syntax-highlighting"
+
 if ! zplug check --verbose; then
   printf "Install zplug plugins? [y/N]: "
   if read -r -q; then
