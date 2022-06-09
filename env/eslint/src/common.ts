2dfaa400fc2c6cb21d927703e3196783ff17063d
SlashNephy <spica@starry.blue>
2022-06-09 23:49:28 +0900

    eslint-config, prettier-config を設置

---
+++ env/eslint/src/common.ts
@@ -0,0 +1,12 @@
+import type { Linter } from 'eslint'
+
+/**
+ * 言語・フレームワークに関わらない eslint プリセット
+ */
+const common: Linter.Config = {
+  extends: ['eslint:recommended'],
+  // 不要なルール無効化コメントを報告
+  reportUnusedDisableDirectives: true,
+}
+
+module.exports = common
---
+++ env/prettier/src/index.ts
@@ -0,0 +1,10 @@
+import type { Options } from 'prettier'
+
+const prettier: Options = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  endOfLine: 'lf',
+}
+
+module.exports = prettier
