f393607d6f5510370f5bbc20fc126c2a43f8ea0e
SlashNephy <spica@starry.blue>
2022-06-10 21:38:15 +0900

    stylelint-config-recommended 追加

--- env/stylelint/src/index.ts
+++ env/stylelint/src/index.ts
@@ -1,7 +1,10 @@
 import type { Config } from 'stylelint'
 
 const config: Config = {
-  extends: ['stylelint-config-property-sort-order-smacss'],
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-property-sort-order-smacss',
+  ],
 }
 
 module.exports = config
