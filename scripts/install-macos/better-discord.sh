577b7fe921659e9ab80440635fee24f22faf909d
SlashNephy <spica@starry.blue>
2022-06-10 20:01:35 +0900

    ワークフロー修正

--- scripts/install-macos/better-discord.sh
+++ scripts/install-macos/better-discord.sh
@@ -1,4 +1,6 @@
 if prompt "Install BetterDiscord plugins & themes?"; then
+  mkdir -p "$HOME/Library/Application Support/BetterDiscord"
+
   ln -sf "$ENV_DIR/discord/plugins" "$HOME/Library/Application Support/BetterDiscord/plugins"
   ln -sf "$ENV_DIR/discord/themes" "$HOME/Library/Application Support/BetterDiscord/themes"
 fi
