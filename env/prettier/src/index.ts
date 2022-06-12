ec11276519d0c56d963f0904cfb468468e545e3a
SlashNephy <spica@starry.blue>
2022-06-12 16:38:44 +0900

    @slashnephy/prettier-config 0.1.1

--- env/prettier/src/index.ts
+++ env/prettier/src/index.ts
@@ -1,10 +1,20 @@
+
 import type { Options } from 'prettier'
 
 const prettier: Options = {
   semi: false,
   singleQuote: true,
+  jsxSingleQuote: false,
   trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  proseWrap: 'preserve',
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'css',
   endOfLine: 'lf',
+  quoteProps: 'as-needed',
+  embeddedLanguageFormatting: 'auto',
+  singleAttributePerLine: false,
 }
 
 module.exports = prettier
