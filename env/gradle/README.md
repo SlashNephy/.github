9079ec701d45b12891e95e60051c77a91e5c9152
SlashNephy <spica@starry.blue>
2022-06-13 14:25:28 +0900

    env/gradle

---
+++ env/gradle/README.md
@@ -0,0 +1,15 @@
+# gradle-extension
+
+🐘 A Gradle plugin which extends the existing Gradle Kotlin DSL.
+
+[Gradle - Plugin: blue.starry.gradle](https://plugins.gradle.org/plugin/blue.starry.gradle)
+
+## Publish
+
+API keys are available at https://plugins.gradle.org.
+
+```console
+$ ./gradlew publishPlugins \
+    -Pgradle.publish.key=${GRADLE_PUBLISH_KEY} \
+    -Pgradle.publish.secret=${GRADLE_PUBLISH_SECRET}
+```
---
+++ env/gradle/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    kotlin("jvm") version "1.7.0"
+
+    `java-gradle-plugin`
+    `maven-publish`
+    id("com.gradle.plugin-publish") version "0.21.0"
+}
+
+group = "blue.starry"
+version = "0.0.1"
+
+pluginBundle {
+    website = "https://github.com/SlashNephy/.github/tree/master/env/gradle"
+    vcsUrl = "https://github.com/SlashNephy/.github"
+    tags = listOf("kotlin", "extension")
+}
+
+gradlePlugin {
+    plugins {
+        create("gradle-extension") {
+            id = "blue.starry.gradle"
+            displayName = "gradle-extension"
+            description = "A Gradle plugin which extends the existing Gradle Kotlin DSL."
+            implementationClass = "blue.starry.gradle.GradleExtensionPlugin"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        mavenLocal()
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(gradleApi())
+    implementation(kotlin("stdlib"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_17.toString()
+        }
+    }
+}
