86aa0c0471ce4bb4d192cad686970535ef451432
SlashNephy <spica@starry.blue>
2022-06-21 13:58:40 +0900

    env/wsl

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,6 +1,4 @@
 $env:ENV_DIR = "$($env:USERPROFILE)\.github\env"
-$env:ENV_COMMON_DIR = Join-Path $env:ENV_DIR "common"
-$env:ENV_WINDOWS_DIR = Join-Path $env:ENV_DIR "windows"
 
 foreach ($item in Get-ChildItem ~\.github\scripts\install-windows\*.ps1)
 {
