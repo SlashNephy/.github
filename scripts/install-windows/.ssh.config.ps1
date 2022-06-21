4b4d1e881eecb5f1a801bb310cd9def092914363
SlashNephy <spica@starry.blue>
2022-06-21 13:44:09 +0900

    env/ssh

--- scripts/install-windows/.ssh.config.ps1
+++ scripts/install-windows/.ssh.config.ps1
@@ -1,4 +1,4 @@
 $ssh_dir = Join-Path $env:USERPROFILE ".ssh"
 
 New-Item -ItemType Directory -Path $ssh_dir -ErrorAction SilentlyContinue
-New-Item -ItemType SymbolicLink -Path (Join-Path $ssh_dir "config") -Target (Join-Path $env:ENV_COMMON_DIR ".ssh" | Join-Path -ChildPath "config") -Force
+New-Item -ItemType SymbolicLink -Path (Join-Path $ssh_dir "config") -Target (Join-Path $env:ENV_DIR "ssh" | Join-Path -ChildPath "config") -Force
