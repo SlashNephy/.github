0a6213b58cf95135579697622748b6cfcaf9a3f7
SlashNephy <spica@starry.blue>
2022-07-05 20:55:31 +0900

    @slashnephy/typescript-extension 0.2.3

--- env/typescript/src/array/min.ts
+++ env/typescript/src/array/min.ts
@@ -1,22 +1,53 @@
 export {}
 
 declare global {
-  // eslint-disable-next-line unused-imports/no-unused-vars
   interface Array<T = number> {
-    min(): number
+    min(): T
+    minOrNull(): T | null
   }
 
   interface Array<T> {
-    minBy(key: (item: T) => number): number
+    minBy<R = number>(key: (item: T) => R): T
+    minByOrNull<R = number>(key: (item: T) => R): T | null
   }
 }
 
 Array.prototype.min = function <T = number>(): number {
+  if (this.length === 0) {
+    throw new Error('Array is empty')
+  }
+
   const array = this as T[] as unknown as number[]
   return Math.min(...array)
 }
 
-Array.prototype.minBy = function <T>(key: (item: T) => number): number {
+Array.prototype.minOrNull = function <T = number>(): number | null {
+  if (this.length === 0) {
+    return null
+  }
+
+  const array = this as T[] as unknown as number[]
+  return Math.min(...array)
+}
+
+Array.prototype.minBy = function <T, R = number>(key: (item: T) => R): T {
+  if (this.length === 0) {
+    throw new Error('Array is empty')
+  }
+
   const array = this.map((item) => key(item))
-  return array.min()
+  const min = array.min()
+  return this.find((item) => key(item) === min)
+}
+
+Array.prototype.minByOrNull = function <T, R = number>(
+  key: (item: T) => R
+): T | null {
+  if (this.length === 0) {
+    return null
+  }
+
+  const array = this.map((item) => key(item))
+  const min = array.min()
+  return this.find((item) => key(item) === min)
 }
