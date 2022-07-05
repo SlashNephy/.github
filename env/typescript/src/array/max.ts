5161ebafdc2551a32d3cdd907d45fc6eef54a2c7
SlashNephy <spica@starry.blue>
2022-07-05 20:55:31 +0900

    @slashnephy/typescript-extension 0.2.3

--- env/typescript/src/array/max.ts
+++ env/typescript/src/array/max.ts
@@ -1,22 +1,53 @@
 export {}
 
 declare global {
-  // eslint-disable-next-line unused-imports/no-unused-vars
   interface Array<T = number> {
-    max(): number
+    max(): T
+    maxOrNull(): T | null
   }
 
   interface Array<T> {
-    maxBy(key: (item: T) => number): number
+    maxBy<R = number>(key: (item: T) => R): T
+    maxByOrNull<R = number>(key: (item: T) => R): T | null
   }
 }
 
 Array.prototype.max = function <T = number>(): number {
+  if (this.length === 0) {
+    throw new Error('Array is empty')
+  }
+
   const array = this as T[] as unknown as number[]
   return Math.max(...array)
 }
 
-Array.prototype.maxBy = function <T>(key: (item: T) => number): number {
+Array.prototype.maxOrNull = function <T = number>(): number | null {
+  if (this.length === 0) {
+    return null
+  }
+
+  const array = this as T[] as unknown as number[]
+  return Math.max(...array)
+}
+
+Array.prototype.maxBy = function <T, R = number>(key: (item: T) => R): T {
+  if (this.length === 0) {
+    throw new Error('Array is empty')
+  }
+
   const array = this.map((item) => key(item))
-  return array.max()
+  const max = array.max()
+  return this.find((item) => key(item) === max)
+}
+
+Array.prototype.maxByOrNull = function <T, R = number>(
+  key: (item: T) => R
+): T | null {
+  if (this.length === 0) {
+    return null
+  }
+
+  const array = this.map((item) => key(item))
+  const max = array.max()
+  return this.find((item) => key(item) === max)
 }
