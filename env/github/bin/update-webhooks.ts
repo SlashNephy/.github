fafd2656ffbecff367ce7d472848f537a4ddbd1f
SlashNephy <spica@starry.blue>
2022-07-14 00:05:47 +0900

    CI で webhook 更新を掛ける

--- env/github/bin/update-webhooks.ts
+++ env/github/bin/update-webhooks.ts
@@ -1,60 +1,38 @@
 import { execSync } from 'child_process'
 
-import { Octokit } from '@octokit/rest'
-import dotenv from 'dotenv'
-
-import type { RestEndpointMethodTypes } from '@octokit/rest'
-
-type ListReposResponseData =
-  RestEndpointMethodTypes['repos']['listForUser']['response']['data'][0]
-type ListWebhooksResponseData =
-  RestEndpointMethodTypes['repos']['listWebhooks']['response']['data'][0]
-
-dotenv.config()
-
-const { GITHUB_TOKEN, WEBHOOK_URL } = process.env
-if (!GITHUB_TOKEN || !WEBHOOK_URL) {
-  throw new Error('Environment values are not set')
-}
-
-const octokit = new Octokit({
-  auth: GITHUB_TOKEN,
-})
-
-const listRepos = async (
-  username: string
-): Promise<ListReposResponseData[]> => {
-  return await octokit.paginate(octokit.repos.listForUser, {
-    username,
-    per_page: 100,
-  })
-}
-
-const listRepoWebhooks = async (
-  owner: string,
-  repo: string
-): Promise<ListWebhooksResponseData[]> => {
-  const { data } = await octokit.repos.listWebhooks({
-    owner,
-    repo,
-  })
-  return data
-}
+import { env } from '../lib/env'
+import {
+  listOrgRepos,
+  listRepoWebhooks,
+  listUserRepos,
+  octokit,
+} from '../lib/api'
 
 const main = async () => {
-  const repos = await listRepos('SlashNephy')
+  const repos = await Promise.all([
+    listUserRepos('SlashNephy'),
+    listOrgRepos('StarryBlueSky'),
+  ]).then((result) => result.flat())
+
   for (const repo of repos) {
     const webhooks = await listRepoWebhooks(repo.owner.login, repo.name)
 
+    let webhookUrl: string
+    if (repo.owner.login === 'SlashNephy') {
+      webhookUrl = env.USER_WEBHOOK_URL
+    } else {
+      webhookUrl = env.ORG_WEBHOOK_URL
+    }
+
     let found = false
     for (const webhook of webhooks) {
-      if (webhook.config.url === WEBHOOK_URL) {
+      if (webhook.config.url === webhookUrl) {
         found = true
         continue
       }
 
       execSync(
-        `curl -s -X DELETE -H "Accept: application/vnd.github+json" -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/${repo.owner.login}/${repo.name}/hooks/${webhook.id}`
+        `curl -s -X DELETE -H "Accept: application/vnd.github+json" -H "Authorization: token ${env.GITHUB_TOKEN}" https://api.github.com/repos/${repo.owner.login}/${repo.name}/hooks/${webhook.id}`
       )
       // FIXME: なぜか octokit が使えないので curl で代用している
       // await octokit.repos.deleteWebhook({
@@ -62,8 +40,9 @@ const main = async () => {
       //   repo: repo.name,
       //   hook_id: webhook.id,
       // })
+
       console.info(
-        `[${repo.owner.login}/${repo.name}] Deleted webhook: ${webhook.config.url}`
+        `[${repo.owner.login}/${repo.name}] Deleted webhook: ${webhook.id}`
       )
     }
 
@@ -75,11 +54,12 @@ const main = async () => {
         active: true,
         events: ['*'],
         config: {
-          url: WEBHOOK_URL,
+          url: webhookUrl,
           content_type: 'json',
           insecure_ssl: '0',
         },
       })
+
       console.info(`[${repo.owner.login}/${repo.name}] Created webhook`)
     }
   }
