5a91e2e9cfb9b4cefcb8d8f78d490ccfbaa8e662
SlashNephy <spica@starry.blue>
2022-07-26 19:01:19 +0900

    AMQのvideoをpreload=autoするスクリプトを追加

--- env/userscript/rollup.config.ts
+++ env/userscript/rollup.config.ts
@@ -3,9 +3,9 @@ import { buildOptions } from './lib/tampermonkey'
 import type { RollupOptions } from 'rollup'
 
 const config: RollupOptions[] = [
-  buildOptions('annict-work-links', {
+  buildOptions({
+    id: 'annict-work-links',
     name: 'Annict Work Links',
-    namespace: 'https://annict.com/',
     version: '0.2.0',
     description: 'しょぼいカレンダー、MyAnimeList、AniList に対応しています。',
     author: 'SlashNephy <spica@starry.blue>',
@@ -13,30 +13,40 @@ const config: RollupOptions[] = [
     connect: 'raw.githubusercontent.com',
     grant: 'GM_xmlhttpRequest',
   }),
-  buildOptions('dominion-online-auto-table-setter', {
+  buildOptions({
+    id: 'dominion-online-auto-table-setter',
     name: 'dominion.games Auto Table Setter',
-    namespace: 'https://dominion.games/',
     version: '0.3.0',
     description: 'dominion.games の卓設定を自動的に設定します。',
     author: 'SlashNephy <spica@starry.blue>',
     match: 'https://dominion.games/',
   }),
-  buildOptions('ff14angler-bypass-adblock-check', {
+  buildOptions({
+    id: 'ff14angler-bypass-adblock-check',
     name: 'FF14Angler Bypass AdBlock Check',
-    namespace: 'https://ff14angler.com/',
     version: '0.2.0',
     description: 'FF14Angler の AdBlock チェックを無効にします。',
     author: 'SlashNephy <spica@starry.blue>',
     match: 'https://*.ff14angler.com/*',
     runAt: 'document-body',
   }),
-  buildOptions('feedly-auto-refresh', {
+  buildOptions({
+    id: 'feedly-auto-refresh',
     name: 'Feedly Auto Refresh',
-    namespace: 'https://feedly.com/',
     version: '0.2.0',
     description: 'Feedly で未読記事がないとき、フィードを自動リフレッシュします。',
     author: 'SlashNephy <spica@starry.blue>',
     match: 'https://feedly.com/i/collection/*',
+  }),
+  buildOptions({
+    id: 'amq-preload-video',
+    name: 'AMQ Preload Video',
+    version: '0.1.0',
+    description: 'Just enable media preloading. Speed up buffering.',
+    author: 'SlashNephy <spica@starry.blue>',
+    match: 'https://animemusicquiz.com/',
+    requires: ['https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqScriptInfo.js'],
+    icon: 'https://animemusicquiz.com/favicon-32x32.png',
   }),
 ]
 
