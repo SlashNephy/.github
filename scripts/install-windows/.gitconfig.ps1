0c895ef7cb8db4bcae246d6aa93f631cd108759f
SlashNephy <spica@starry.blue>
2022-08-03 00:41:09 +0900

    Update .gitconfig.ps1

--- scripts/install-windows/.gitconfig.ps1
+++ scripts/install-windows/.gitconfig.ps1
@@ -1,2 +1,2 @@
 $git_dir = Join-Path $env:ENV_DIR "git"
-New-Item -ItemType SymbolicLink -Path (Join-Path $env:USERPROFILE ".gitconfig") -Target (Join-Path $git_dir ".gitconfig") -Force
+New-Item -ItemType SymbolicLink -Path (Join-Path $env:USERPROFILE ".gitconfig") -Target (Join-Path $git_dir "windows.gitconfig") -Force
