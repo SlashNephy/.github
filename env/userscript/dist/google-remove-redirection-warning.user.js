2687fe370b57e1a4d0d142399cc933397a60c1c0
SlashNephy <SlashNephy@users.noreply.github.com>
2022-08-19 04:54:23 +0000

    ⚙️ Build (env/userscript)

--- env/userscript/dist/google-remove-redirection-warning.user.js
+++ env/userscript/dist/google-remove-redirection-warning.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Google Remove Redirection Warning
 // @namespace    https://tampermonkey.net/
-// @version      0.1.0
+// @version      0.1.1
 // @description  Remove redirection warning from https://google.com/url
 // @author       SlashNephy <spica@starry.blue>
 // @match        https://google.com/url?q=*
+// @match        https://www.google.com/url?q=*
 // @license      MIT license
 // @grant        none
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
