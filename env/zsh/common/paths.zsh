4496f4a6550e3d8ac1a4441d18479429b9e28c2a
SlashNephy <spica@starry.blue>
2022-09-02 11:40:08 +0900

    Sync

---
+++ env/zsh/common/paths.zsh
@@ -0,0 +1,2 @@
+mkdir -p "$HOME/.bin"
+export PATH="$HOME/.bin:$PATH"
--- env/zsh/macos.zsh
+++ env/zsh/macos.zsh
@@ -4,9 +4,6 @@ source "$HOME/.github/env/zsh/common.zsh"
 if [ -f "/opt/homebrew/bin/brew" ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
-if [ -f "/usr/local/bin/brew" ]; then
-  eval "$(/usr/local/bin/brew shellenv)"
-fi
 
 HOMEBREW_PREFIX=$(brew --prefix)
 export HOMEBREW_PREFIX
