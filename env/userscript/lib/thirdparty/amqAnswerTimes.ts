18b8cdb43dfc7dc27dab73e4e37c1b5427c32480
SlashNephy <spica@starry.blue>
2022-09-22 21:43:36 +0900

    AmqAnswerTimesUtility をロールバック

--- env/userscript/lib/thirdparty/amqAnswerTimes.ts
+++ env/userscript/lib/thirdparty/amqAnswerTimes.ts
@@ -3,39 +3,36 @@
 import type { JoinGameEvent, PlayerAnsweredEvent } from '../../types/amq'
 
 class AmqAnswerTimesUtility {
-  private songStartTime = 0
-  private readonly times: number[] = []
+  public songStartTime = 0
+  public playerTimes: number[] = []
 
   public constructor() {
     if (unsafeWindow.Listener === undefined) {
-      throw new Error('Listener is not defined.')
+      return
     }
 
-    new unsafeWindow.Listener('play next song', this._onPlayNextSong).bindListener()
-    new unsafeWindow.Listener('player answered', this._onPlayerAnswered).bindListener()
-    new unsafeWindow.Listener('Join Game', this._onJoinGame).bindListener()
+    new unsafeWindow.Listener('play next song', () => {
+      this.songStartTime = Date.now()
+      this.playerTimes = []
+    }).bindListener()
+
+    new unsafeWindow.Listener<PlayerAnsweredEvent>('player answered', (data) => {
+      const time = Date.now() - this.songStartTime
+      data.forEach((gamePlayerId) => {
+        this.playerTimes[gamePlayerId] = time
+      })
+    }).bindListener()
+
+    new unsafeWindow.Listener<JoinGameEvent>('Join Game', (data) => {
+      const quizState = data.quizState
+      if (quizState.songTimer > 0) {
+        this.songStartTime = Date.now() - quizState.songTimer * 1000
+      }
+    }).bindListener()
   }
 
   public query(playerId: number): number | null {
-    return playerId in this.times ? this.times[playerId] : null
-  }
-
-  private _onPlayNextSong() {
-    this.songStartTime = Date.now()
-    this.times.splice(0)
-  }
-
-  private _onPlayerAnswered(playerIds: PlayerAnsweredEvent) {
-    const time = Date.now() - this.songStartTime
-    for (const id of playerIds) {
-      this.times[id] = time
-    }
-  }
-
-  private _onJoinGame({ quizState }: JoinGameEvent) {
-    if (quizState.songTimer > 0) {
-      this.songStartTime = Date.now() - quizState.songTimer * 1000
-    }
+    return playerId in this.playerTimes ? this.playerTimes[playerId] : null
   }
 }
 
