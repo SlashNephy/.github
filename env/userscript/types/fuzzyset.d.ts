8d56b1c699301336253cf241f189a7887976bd8b
SlashNephy <spica@starry.blue>
2022-10-02 12:00:49 +0900

    FuzzySet 入れる

---
+++ env/userscript/types/fuzzyset.d.ts
@@ -0,0 +1,4 @@
+declare global {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-imports,@typescript-eslint/naming-convention
+  declare const FuzzySet: typeof import('fuzzyset')
+}
