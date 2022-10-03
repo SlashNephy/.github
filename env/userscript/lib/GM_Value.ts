c5b378d56e92f1c6dda68474cf5e7572e1856fd5
SlashNephy <spica@starry.blue>
2022-10-03 20:35:07 +0900

    名前変える

--- env/userscript/lib/GM_Value.ts
+++ env/userscript/lib/GM_Value.ts
@@ -1,25 +1,28 @@
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export class GM_Value<T> {
-  public constructor(
-    private readonly _key: string,
-    private readonly _default?: T,
-    private readonly _initialize: boolean = true
-  ) {
-    const value = GM_getValue<T | null>(_key, null)
-    if (_initialize && value === null) {
-      GM_setValue(_key, _default)
+  private readonly key: string
+  private readonly defaultValue: T | undefined
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  public constructor(key: string, defaultValue?: T, initialize = true) {
+    this.key = key
+    this.defaultValue = defaultValue
+
+    const value = GM_getValue<T | null>(key, null)
+    if (initialize && value === null) {
+      GM_setValue(key, defaultValue)
     }
   }
 
   public get(): T {
-    return GM_getValue<T>(this._key, this._default)
+    return GM_getValue<T>(this.key, this.defaultValue)
   }
 
   public set(value: T): void {
-    GM_setValue(this._key, value)
+    GM_setValue(this.key, value)
   }
 
   public delete(): void {
-    GM_deleteValue(this._key)
+    GM_deleteValue(this.key)
   }
 }
