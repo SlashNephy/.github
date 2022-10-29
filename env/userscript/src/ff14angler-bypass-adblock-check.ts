12c12f745c8f7bc8f6c05b52b24418c22c079f0a
SlashNephy <spica@starry.blue>
2022-10-29 22:33:16 +0900

    eslint ルールを更新

--- env/userscript/src/ff14angler-bypass-adblock-check.ts
+++ env/userscript/src/ff14angler-bypass-adblock-check.ts
@@ -1,4 +1,4 @@
 document.head.className = 'adsbygoogle'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/no-unsafe-member-access
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 ;(unsafeWindow as any).$.prototype.remove = () => void 0
