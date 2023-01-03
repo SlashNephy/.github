d7bafa3261023cd7522c8c3581b18f140b4e73ea
SlashNephy <spica@starry.blue>
2023-01-03 14:00:18 +0900

    GitHub Workflow に対してはシングルクォートにする

--- env/eslint/src/yaml.ts
+++ env/eslint/src/yaml.ts
@@ -6,6 +6,16 @@ const yaml: Linter.Config = {
   rules: {
     'yml/quotes': ['error', { prefer: 'double' }],
   },
+  overrides: [
+    // GitHub Workflow でダブルクォートが使えない場合があるのでシングルに統一
+    // https://github.com/actions/runner/issues/866
+    {
+      files: '.github/workflows/*.{yml,yaml}',
+      rules: {
+        'yml/quotes': ['error', { prefer: 'single' }],
+      },
+    },
+  ],
 }
 
 module.exports = yaml
