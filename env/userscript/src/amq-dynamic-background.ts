6afc3f9cd73fc51e22857600061472431a34d2bb
SlashNephy <spica@starry.blue>
2023-02-06 19:59:09 +0900

    Firefox でも動くように

--- env/userscript/src/amq-dynamic-background.ts
+++ env/userscript/src/amq-dynamic-background.ts
@@ -1,73 +1,43 @@
 import { onReady } from '../lib/amq/onReady'
 
-/* eslint-disable @typescript-eslint/naming-convention */
-const CANVAS_UPDATE_INTERVAL = 1000 / 30,
-  CANVAS_WIDTH = 1280,
-  CANVAS_HEIGHT = 720,
-  CANVAS_FILTER = 'blur(2px)'
-/* eslint-enable @typescript-eslint/naming-convention */
-
 onReady(() => {
-  const canvas = document.createElement('canvas')
-  const ctx = canvas.getContext('2d')
-  if (ctx === null) {
-    throw new Error('Your browser does not support CanvasRenderingContext2D')
-  }
-
-  canvas.width = CANVAS_WIDTH
-  canvas.height = CANVAS_HEIGHT
-  canvas.style.position = 'fixed'
-  canvas.style.top = '0'
-  canvas.style.left = '0'
-  canvas.style.width = '100%'
-  canvas.style.height = '100%'
-  document.body.insertAdjacentElement('afterbegin', canvas)
-
   const video = document.createElement('video')
-  video.style.display = 'none'
+  video.id = 'dynamic-background-video'
   video.muted = true
   video.loop = true
-  document.body.insertAdjacentElement('afterbegin', video)
 
-  const getCurrentQuizVideo = (): [HTMLVideoElement, boolean] => {
+  const container = document.getElementById('quizPage') ?? document.body
+  container.insertAdjacentElement('afterbegin', video)
+
+  new Listener('answer results', () => {
     const quizPlayer = unsafeWindow.quizVideoController.getCurrentPlayer()
-    if (quizPlayer === undefined || quizPlayer.player.hasClass('vjs-hidden')) {
-      return [video, false]
+    if (quizPlayer === undefined) {
+      return
     }
 
-    return [quizPlayer.$player[0], true]
-  }
-
-  setInterval(() => {
-    const [quizVideo, isQuizVideoPlayable] = getCurrentQuizVideo()
-
-    if (isQuizVideoPlayable) {
-      // パフォーマンスのため参照しないときは止めておく
-      video.pause()
-
-      // mp3 の時は video をコピーしない
-      if (!quizVideo.src.endsWith('.mp3')) {
-        if (video.src !== quizVideo.src) {
-          video.src = quizVideo.src
-        }
-
-        video.currentTime = quizVideo.currentTime
-      }
-    } else {
-      // video が未設定の場合は draw しない
-      if (video.src === '') {
-        return
-      }
-
-      // video をリジュームする
-      if (video.paused) {
-        void video.play()
-      }
+    // Sound Only の時は video をコピーしない
+    if (quizPlayer.player.isAudio()) {
+      return
     }
 
-    ctx.filter = CANVAS_FILTER
-    ctx.drawImage(quizVideo, 0, 0, canvas.width, canvas.height)
-  }, CANVAS_UPDATE_INTERVAL)
+    const quizVideo = quizPlayer.$player[0]
+    if (video.src !== quizVideo.src) {
+      video.src = quizVideo.src
+      video.currentTime = quizVideo.currentTime
+      void video.play()
+    }
+  }).bindListener()
+
+  AMQ_addStyle(`
+    #dynamic-background-video {
+      position: fixed;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+    }
+  `)
 
   AMQ_addScriptData({
     name: 'Dynamic Background',
