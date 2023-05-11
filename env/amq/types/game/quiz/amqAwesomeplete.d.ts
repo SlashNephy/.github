a201b74b9596ee46fcf4d885a6321ad36ee2ca8c
SlashNephy <spica@starry.blue>
2023-05-11 12:46:15 +0900

    Sync

--- env/amq/types/game/quiz/amqAwesomeplete.d.ts
+++ env/amq/types/game/quiz/amqAwesomeplete.d.ts
@@ -1,22 +1,22 @@
-declare function AmqAwesomeplete(input: any, o: any, scrollable: any): void;
+declare function AmqAwesomeplete(input: any, o: any, scrollable: any): void
 declare class AmqAwesomeplete {
-    constructor(input: any, o: any, scrollable: any);
-    searchId: number;
-    currentSubList: any;
-    letterLists: {};
-    currentQuery: string;
-    $ul: JQuery<any>;
-    item: (text: any, input: any, item_id: any) => any;
-    evaluate(): void;
-    index: number;
-    hide(): void;
-    goto(i: any): void;
+  constructor(input: any, o: any, scrollable: any)
+  searchId: number
+  currentSubList: any
+  letterLists: {}
+  currentQuery: string
+  $ul: JQuery<any>
+  item: (text: any, input: any, item_id: any) => any
+  evaluate(): void
+  index: number
+  hide(): void
+  goto(i: any): void
 }
-declare function Suggestion(data: any): void;
+declare function Suggestion(data: any): void
 declare class Suggestion {
-    constructor(data: any);
-    label: any;
-    value: any;
-    toString: () => string;
-    valueOf(): string;
+  constructor(data: any)
+  label: any
+  value: any
+  toString: () => string
+  valueOf(): string
 }
--- env/amq/types/gameSettings/advancedSettingController.d.ts
+++ env/amq/types/gameSettings/advancedSettingController.d.ts
@@ -1,8 +1,13 @@
-declare function AdvancedSettingController($switch: any, $standardSettingContainer: any, $advacnedSettingContainer: any, doubleActive: any): void;
+declare function AdvancedSettingController(
+  $switch: any,
+  $standardSettingContainer: any,
+  $advacnedSettingContainer: any,
+  doubleActive: any
+): void
 declare class AdvancedSettingController {
-    constructor($switch: any, $standardSettingContainer: any, $advacnedSettingContainer: any, doubleActive: any);
-    $standardContainer: any;
-    $advancedContainer: any;
-    doubleActive: any;
-    setOn(on: any): void;
+  constructor($switch: any, $standardSettingContainer: any, $advacnedSettingContainer: any, doubleActive: any)
+  $standardContainer: any
+  $advancedContainer: any
+  doubleActive: any
+  setOn(on: any): void
 }
