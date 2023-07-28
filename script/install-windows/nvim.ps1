f577b0735819f9cb8bb8b8878b9ebc178b95edf8
SlashNephy <spica@starry.blue>
2023-07-28 19:48:00 +0900

    fix installation script

--- script/install-windows/nvim.ps1
+++ script/install-windows/nvim.ps1
@@ -1,1 +1,5 @@
-git clone --depth 1 https://github.com/AstroNvim/AstroNvim $env:LOCALAPPDATA\nvim
+$nvim_dir = Join-Path $env:LOCALAPPDATA "nvim"
+
+If (-not (Test-Path $nvim_dir)) {
+    git clone --depth 1 https://github.com/AstroNvim/AstroNvim $nvim_dir
+}
--- script/install.ps1
+++ script/install.ps1
@@ -2,5 +2,6 @@ $env:ENV_DIR = "$($env:USERPROFILE)\.github\env"
 
 foreach ($item in Get-ChildItem ~\.github\script\install-windows\*.ps1)
 {
+    Write-Host $item.Name
     Invoke-Expression $item.FullName -ErrorAction Stop
 }
