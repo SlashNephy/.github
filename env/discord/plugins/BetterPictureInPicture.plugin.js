b73ce9e4e35c4a6a826ca26d4923a475a99f7335
SlashNephy <spica@starry.blue>
2023-08-06 21:17:37 +0900

    Sync

--- env/discord/plugins/BetterPictureInPicture.plugin.js
+++ env/discord/plugins/BetterPictureInPicture.plugin.js
@@ -1,8 +1,10 @@
 /**
  * @name BetterPictureInPicture
- * @invite 3ts2znePu7
+ * @description Simple plugin that allows you to resize the picture-in-picture popup using the mouse wheel and the settings.
+ * @version 0.0.4
+ * @author nik9
+ * @authorId 241175583709593600
  * @authorLink https://megaworld.space
- * @donate https://vk.com/app6887721_-197274096
  * @website https://github.com/nik9play/BetterDiscordPlugins/tree/main/Plugins/BetterPictureInPicture
  * @source https://raw.githubusercontent.com/nik9play/BetterDiscordPlugins/main/Releases/BetterPictureInPicture.plugin.js
  */
@@ -29,33 +31,129 @@
     WScript.Quit();
 
 @else@*/
-
-module.exports = (() => {
-    const config = {"main":"index.js","info":{"name":"BetterPictureInPicture","authors":[{"name":"nik9","discord_id":"241175583709593600","github_username":"nik9play"}],"version":"0.0.3","description":"Allows you to resize the Picture-in-Picture popup with mouse wheel.","inviteCode":"3ts2znePu7","authorLink":"https://megaworld.space","paypalLink":"https://vk.com/app6887721_-197274096","github":"https://github.com/nik9play/BetterDiscordPlugins/tree/main/Plugins/BetterPictureInPicture","github_raw":"https://raw.githubusercontent.com/nik9play/BetterDiscordPlugins/main/Releases/BetterPictureInPicture.plugin.js"},"changelog":[{"title":"Mouse wheel zoom","items":["Now you can change size of popup with mouse wheel."]},{"title":"Fixes","items":["A lot of fixes."]}],"defaultConfig":[{"type":"slider","id":"popupsize","name":"PiP size","note":"Set the PiP popup size in percent","value":100,"min":100,"max":300,"markers":[100,110,125,150,175,200,250,300],"stickToMarkers":false,"units":"%"},{"type":"switch","id":"hideswitch","name":"Hide the PiP popup","note":"Hide the PiP popup completely","value":false},{"type":"switch","id":"customswitch","name":"Set custom size","note":"Set custom size in pixels","value":false},{"type":"textbox","id":"customwidth","name":"Width","note":"Set the width of popup","value":"320","placeholder":"Size in pixels"},{"type":"textbox","id":"customheight","name":"Height","note":"Set the height of popup","value":"180","placeholder":"Size in pixels"}]};
-
-    return !global.ZeresPluginLibrary ? class {
-        constructor() {this._config = config;}
-        getName() {return config.info.name;}
-        getAuthor() {return config.info.authors.map(a => a.name).join(", ");}
-        getDescription() {return config.info.description;}
-        getVersion() {return config.info.version;}
-        load() {
-            BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
-                confirmText: "Download Now",
-                cancelText: "Cancel",
-                onConfirm: () => {
-                    require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
-                        if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
-                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
+const config = {
+    main: "index.js",
+    info: {
+        name: "BetterPictureInPicture",
+        authors: [
+            {
+                name: "nik9",
+                discord_id: "241175583709593600",
+                github_username: "nik9play"
+            }
+        ],
+        version: "0.0.4",
+        description: "Simple plugin that allows you to resize the picture-in-picture popup using the mouse wheel and the settings.",
+        authorLink: "https://megaworld.space",
+        paypalLink: "https://vk.com/app6887721_-197274096",
+        github: "https://github.com/nik9play/BetterDiscordPlugins/tree/main/Plugins/BetterPictureInPicture",
+        github_raw: "https://raw.githubusercontent.com/nik9play/BetterDiscordPlugins/main/Releases/BetterPictureInPicture.plugin.js"
+    },
+    changelog: [
+        {
+            title: "Fixes",
+            items: [
+                "Fix aspect ratio and tweak animation speed."
+            ]
+        },
+        {
+            title: "Mouse wheel zoom",
+            items: [
+                "Now you can change size of popup with mouse wheel."
+            ]
+        },
+        {
+            title: "Fixes",
+            items: [
+                "A lot of fixes."
+            ]
+        }
+    ],
+    defaultConfig: [
+        {
+            type: "slider",
+            id: "popupsize",
+            name: "PiP size",
+            note: "Set the PiP popup size in percent",
+            value: 100,
+            min: 50,
+            max: 300,
+            markers: [
+                50,
+                75,
+                100,
+                125,
+                150,
+                175,
+                200,
+                250,
+                300
+            ],
+            stickToMarkers: false,
+            units: "%"
+        },
+        {
+            type: "switch",
+            id: "hideswitch",
+            name: "Hide the PiP popup",
+            note: "Hide the PiP popup completely",
+            value: false
+        },
+        {
+            type: "switch",
+            id: "customswitch",
+            name: "Set custom size",
+            note: "Set custom size in pixels",
+            value: false
+        },
+        {
+            type: "textbox",
+            id: "customwidth",
+            name: "Width",
+            note: "Set the width of popup",
+            value: "320",
+            placeholder: "Size in pixels"
+        },
+        {
+            type: "textbox",
+            id: "customheight",
+            name: "Height",
+            note: "Set the height of popup",
+            value: "180",
+            placeholder: "Size in pixels"
+        }
+    ]
+};
+class Dummy {
+    constructor() {this._config = config;}
+    start() {}
+    stop() {}
+}
+ 
+if (!global.ZeresPluginLibrary) {
+    BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.name ?? config.info.name} is missing. Please click Download Now to install it.`, {
+        confirmText: "Download Now",
+        cancelText: "Cancel",
+        onConfirm: () => {
+            require("request").get("https://betterdiscord.app/gh-redirect?id=9", async (err, resp, body) => {
+                if (err) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
+                if (resp.statusCode === 302) {
+                    require("request").get(resp.headers.location, async (error, response, content) => {
+                        if (error) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
+                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), content, r));
                     });
+                }
+                else {
+                    await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
                 }
             });
         }
-        start() {}
-        stop() {}
-    } : (([Plugin, Api]) => {
-        const plugin = (Plugin, Library) => {
-  const { Logger, DOMTools, ReactTools } = Library
+    });
+}
+ 
+module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
+     const plugin = (Plugin, Library) => {
+  const { Logger, DOMTools } = Library
 
   return class BetterPictureInPicture extends Plugin {
 
@@ -67,7 +165,8 @@ module.exports = (() => {
         BdApi.injectCSS('betterpictureinpicturecss-hide', `div[class^="pictureInPictureWindow-"] {display:none!important}`)
       }
 
-      BdApi.injectCSS('betterpictureinpicturecss-animation', `div[class^="pictureInPictureVideo-"] {transition: width .5s cubic-bezier(0.65,0.05,0.36,1), height .5s cubic-bezier(0.65,0.05,0.36,1);}`)
+      BdApi.injectCSS('betterpictureinpicturecss-animation', `div[class^="pictureInPictureVideo-"] {transition: width .2s cubic-bezier(0.65,0.05,0.36,1), height .2s cubic-bezier(0.65,0.05,0.36,1);}`)
+      BdApi.injectCSS('betterpictureinpicturecss', `div[class^="pictureInPictureVideo-"] {width: var(--bpip-width);height:var(--bpip-height)!important}`)
 
       DOMTools.observer.subscribe(changes => {
         if (changes.addedNodes.length > 0) {
@@ -86,14 +185,14 @@ module.exports = (() => {
           let scaleX = parseFloat(this.settings['customwidth'])
           scaleX += e.deltaY * -0.1
           let scaleY = parseFloat(this.settings['customheight'])
-          scaleY += e.deltaY * -0.1
+          scaleY += e.deltaY * -0.05625
           
           this.settings['customwidth'] = scaleX
           this.settings['customheight'] = scaleY
         } else {
           let scale = parseFloat(this.settings['popupsize'])
           scale += e.deltaY * -0.1
-          if (scale < 100) scale = 100
+          if (scale < 50) scale = 50
           if (scale > 300) scale = 300
           this.settings['popupsize'] = scale
         }
@@ -113,6 +212,8 @@ module.exports = (() => {
       BdApi.clearCSS('betterpictureinpicturecss')
       BdApi.clearCSS('betterpictureinpicturecss-animation')
       BdApi.clearCSS('betterpictureinpicturecss-hide')
+      BdApi.clearCSS('betterpictureinpicture-vars')
+
       DOMTools.observer.unsubscribeAll()
 
       const window = DOMTools.query('div[class^="pictureInPictureWindow-"]')
@@ -125,16 +226,14 @@ module.exports = (() => {
 
     setSize() {
       Logger.log('Size changed')
-      BdApi.clearCSS('betterpictureinpicturecss')
+      BdApi.clearCSS('betterpictureinpicture-vars')
       if (this.settings['customswitch']) {
-        BdApi.injectCSS('betterpictureinpicturecss', `div[class^="pictureInPictureVideo-"] {width: ${this.settings['customwidth']}px!important;height:${this.settings['customheight']}px!important}`)
+        BdApi.injectCSS('betterpictureinpicture-vars', `:root {--bpip-width: ${this.settings['customwidth']}px; --bpip-height: ${this.settings['customheight']}px;}`)
       } else {
         const width = 320 * (this.settings['popupsize'] / 100)
         const height = 180 * (this.settings['popupsize'] / 100)
 
-        BdApi.injectCSS('betterpictureinpicturecss', `div[class^="pictureInPictureVideo-"] {width: ${width}px!important;height:${height}px!important}`)
-        // BdApi.injectCSS('betterpictureinpicturecss', `div[class^="pictureInPictureWindow-"] {transform: scale(${this.settings['popupsize'] / 100})!important}`)
-      }
+        BdApi.injectCSS('betterpictureinpicture-vars', `:root {--bpip-width: ${width}px; --bpip-height: ${height}px;}`)      }
     }
 
     getSettingsPanel() {
@@ -160,7 +259,6 @@ module.exports = (() => {
     }
   }
 };
-        return plugin(Plugin, Api);
-    })(global.ZeresPluginLibrary.buildPlugin(config));
-})();
+     return plugin(Plugin, Api);
+})(global.ZeresPluginLibrary.buildPlugin(config));
 /*@end@*/
