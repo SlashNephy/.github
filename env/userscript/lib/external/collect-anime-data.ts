66671bf6a65f98f171e4b1324c7327c9d27471f1
SlashNephy <spica@starry.blue>
2023-09-20 19:56:01 +0900

    SlashNephy/anime-titles-map を使う

--- env/userscript/lib/external/collect-anime-data.ts
+++ env/userscript/lib/external/collect-anime-data.ts
@@ -2,7 +2,7 @@ export type AnimeTitleMap = Record<string, string[]>
 
 export async function fetchAnimeTitles(branch = 'master'): Promise<AnimeTitleMap> {
   const response = await fetch(
-    `https://raw.githubusercontent.com/SlashNephy/.github/${branch}/env/userscript/bin/collect-anime-data/dist/titles.json`
+    `https://raw.githubusercontent.com/SlashNephy/anime-titles-map/${branch}/dist/japanese.json`
   )
   return response.json()
 }
