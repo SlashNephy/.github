3d68cfb772739e355e1b5c632534f216a9f13da6
SlashNephy <slashnephy@hatena.ne.jp>
2024-11-28 11:26:06 +0900

    update

--- env/zsh/common/history.zsh
+++ env/zsh/common/history.zsh
@@ -1,12 +1,18 @@
+# history file
 export HISTFILE=${HOME}/.zsh_history
-export HISTSIZE=1000
-export SAVEHIST=100000
-setopt hist_ignore_dups
-setopt EXTENDED_HISTORY
-setopt hist_expand
+export HISTSIZE=10000 # on-memory
+export SAVEHIST=100000 # file
+setopt append_history # append instead create new history
 setopt inc_append_history
-setopt hist_ignore_all_dups
+setopt share_history
+setopt extended_history # keep timestamp
+setopt hist_reduce_blanks # trim spaces
+setopt hist_save_no_dups # delete same command
 
-function history-all {
-  history -E 1
-}
+setopt hist_verify # editable
+setopt hist_expand
+
+setopt hist_ignore_dups # ignore same command
+setopt hist_ignore_all_dups # ignore duplicated command
+setopt hist_no_store # ignore history command
+setopt hist_ignore_space # ignore empty command
